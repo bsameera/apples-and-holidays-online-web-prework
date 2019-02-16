ca3f08067c530c4a38885c17fcf049c77f60f9c4
splendid-frame-7429 <bsameera@yahoo.com>
2019-02-16 03:20:13 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -69,10 +69,10 @@ def all_supplies_in_holidays(holiday_hash)
     season = season_key.to_s + ':'
     puts season.capitalize
     season_value.each do |key, val|
-      hol_key = key.to_s
+      hol_key = key.to_s.capitalize
       if hol_key.include?('_') 
-        hol_key = hol_key.split('_').map { |x| x.capitalize }
-        hol_key = hol_key.join(' ')
+        hol_key = hol_key.split('_').map { |x| x.capitalize }.join(' ')
+        #hol_key = hol_key.join(' ')
       end 
       hol_value = val.join(', ')
       puts '  ' + hol_key + ':' + ' ' + hol_value
