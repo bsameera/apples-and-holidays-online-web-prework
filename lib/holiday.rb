9663cff09d1a72d20080ae10fa0c54cb780f2d3a
splendid-frame-7429 <bsameera@yahoo.com>
2019-02-16 02:55:55 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -75,9 +75,9 @@ def all_supplies_in_holidays(holiday_hash)
   end
   
   holiday_hash.each do |season_key, season_value|
-    season = season_key.to_s
-    puts season 
     season_value.each do |key, val|
+      season = season_key.to_s
+      puts season
       hol_key = key + ':'
       hol_value = val.join('')
       puts '  ' + hol_key + ' ' + hol_value
